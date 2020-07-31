8c22fe8221214771b234011f6f176055e88f741f
Infinity-Energy <1298348891@qq.com>
2020-07-31 11:45:38 +0800

    finish update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # hole project
 # type <python-project main.py>
-# version <1.3.0-alpha-I> <snapshot>
-# online version <1.3-alpha-I>
+# version <1.3.0-alpha-II> <snapshot>
+# online version <1.3-alpha-II>
 # status <ok> <online>
 # ==========project from Infinity-Energy=========
 import time
