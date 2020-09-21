07868997d5b73a2d265ec409997870976abf1772
Infinity-Energy <1298348891@qq.com>
2020-09-21 18:20:05 +0800

    end version develop

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# version <1.3.1-beta> <pre-release>
+# version <1.3.1-beta-II> <pre-release>
 # ==========project from github organization "Infinity-EFP"=========
 # code by Infinity-Energy
 # art by Felixqwq
